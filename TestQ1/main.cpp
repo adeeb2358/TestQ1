43586f2437b9122c6ef1f0f5cf3d152ec4d7416b
Adeeb Mohammed <adeeb2358@gmail.com>
2020-07-19 21:16:54 +0900

    global match same as ls commend o(m*n) time complexity

--- TestQ1/main.cpp
+++ TestQ1/main.cpp
@@ -7,9 +7,44 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::vector<bool>> initializeMatchTable(std::string fileName,
+                                                    std::string pattern){
+    std::vector<std::vector<bool>> matchTable;
+    for(int i = 0; i < fileName.length() + 1; i++){
+        std::vector<bool> row = {};
+        for (int j = 0; j < pattern.length() + 1; j++) {
+            row.push_back(false);
+        }
+        matchTable.push_back(row);
+    }
+    matchTable[0][0] = true;
+    for (int  j = 1; j < pattern.length()+1; j++) {
+        if(pattern[j-1] == '*'){
+            matchTable[0][j] = matchTable[0][j-1];
+        }
+    }
+    return matchTable;
+}
+
+bool globMatching(std::string fileName, std::string pattern) {
+    auto matchTable = initializeMatchTable(fileName,pattern);
+    for (int i = 1; i < fileName.length()+1; i++) {
+        for(int j = 1; j < pattern.length()+1; j++){
+            if (pattern[j-1] == '*') {
+                matchTable[i][j] = matchTable[i-1][j] || matchTable[i][j-1];
+            }else if(pattern[j-1] == '?' || pattern[j-1] == fileName[i-1]){
+                matchTable[i][j] = matchTable[i-1][j-1];
+            }
+        }
+    }
+    return matchTable[fileName.length()][pattern.length()];
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Program for matching the pattern" << std::endl;
+    std::cout << globMatching("abcdefg", "a*e?g");
     return 0;
 }
